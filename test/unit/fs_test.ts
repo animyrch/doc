c12814e7a6ea3db54f6f71abf39ec8f53f46c606
animyrch <info@traductionenturc.fr>
2020-08-21 14:17:21 +0200

    Cleaned up test creations.

--- test/unit/fs_test.ts
+++ test/unit/fs_test.ts
@@ -1,25 +1,23 @@
 import { assert, assertEquals } from "https://deno.land/std@0.64.0/testing/asserts.ts";
 import { Fs } from "../../src/fs.ts";
 
-/** given fs instantiated, when fs.scanTestFolder is called, then fs should store test folder structure (all subfolders) in a property */
-
 let fsClass:Fs;
-
+const testFolder:string = './test/mocks/testFolder/';
 const given_fsIsInstantiated = () => {
   fsClass = new Fs();
 }
 
 const given_testFolderHasTwoFoldersAndTwoSubfoldersInOneOfThemAndOneFileInOneOfThem = () => {
-  const folder1 = './test/mocks/testFolder/folder1/';
-  const folder2 = './test/mocks/testFolder/folder2/';
+  const folder1 = 'folder1/';
+  const folder2 = '.folder2/';
   const subfolder1 = 'subfolder1/';
   const subfolder2 = 'subfolder2/';
   const file = 'file.ts';
   try {
-    Deno.mkdirSync(folder1+subfolder1, { recursive: true });
-    Deno.mkdirSync(folder1+subfolder2, { recursive: true });
-    Deno.mkdirSync(folder2, { recursive: true });
-    Deno.createSync(folder1+subfolder1+file);
+    Deno.mkdirSync(testFolder+folder1+subfolder1, { recursive: true });
+    Deno.mkdirSync(testFolder+folder1+subfolder2, { recursive: true });
+    Deno.mkdirSync(testFolder+folder2, { recursive: true });
+    Deno.createSync(testFolder+folder1+subfolder1+file);
   }catch(err){
     console.log('mock folders could not be created or already exists');
   }
@@ -37,13 +35,18 @@ const then_fsShouldStoreTestFolderStructureForExistingFiles = () => {
   assertEquals(fsClass.folderStructure, expectedFolderStructure);
 }
 
+const cleanup_scanFolderTest = async () => {
+  await Deno.remove(testFolder, { recursive: true });
+}
+
 Deno.test({
-  name: "FS scanTestFolder",
+  name: "FS/scanFolder",
   async fn() {
     given_fsIsInstantiated();
     given_testFolderHasTwoFoldersAndTwoSubfoldersInOneOfThemAndOneFileInOneOfThem();
     await when_scanFolder_isCalled();
     then_fsShouldStoreTestFolderStructureForExistingFiles();
+    cleanup_scanFolderTest();
   },
   sanitizeResources: false,
   sanitizeOps: false
